1c16f185e43450a62e91154a046250ed00efdf02
github username <github email address>
2021-06-21 16:51:49 -0400

    grab latitude and longitude from browser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid={API key}'
-
 class App extends Component{
   state = {
     fetchingData: true,
     weatherData: []
+  }
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords
+      console.log(latitude)
+      console.log(longitude)
+      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`)
+    });
   }
 
   render(){
